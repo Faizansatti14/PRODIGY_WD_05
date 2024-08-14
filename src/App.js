797ea38baa4fa0705e402e454d33b8b9dafffad5
Faizansatti14 <sattifaizan053@gmail.com>
2024-08-14 17:16:10 +0500

    functionality

--- src/App.js
+++ src/App.js
@@ -1,22 +1,54 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+const api= {
+  key: "7aab7eca768de428dfe952a48ddcd85f",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
+
 function App() {
+  const [search, setSearch] = useState("");
+  const [weather,setweather]=useState({});
+
+  const searchPressed=()=>{
+    fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+    .then(res=>res.json())
+    .then(results=>{
+      setweather(results);
+
+    })
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/*header*/}
+        <h1>Weather App</h1>
+
+        {/*Seatch box */}
+        <div className="searchbox">
+        <input type="text" placeholder="Search City" onChange={(e)=>setSearch(e.target.value)}/>
+        <button onClick={searchPressed}>search</button>
+        </div>
+        
+        
+        
+        {weather.name && weather.sys && (
+          <>
+            <p>{weather.name}, {weather.sys.country}</p>
+            {weather.main && <p>{Math.round(weather.main.temp)}°C</p>}
+            {weather.weather && weather.weather[0] && (
+              <>
+                <p>Condition: {weather.weather[0].main}</p>
+                <p>Description: {weather.weather[0].description}</p>
+              </>
+            )}
+            {weather.main && <p>Humidity: {weather.main.humidity}%</p>}
+            {weather.wind && <p>Wind Speed: {weather.wind.speed} m/s</p>}
+          </>
+        )}
+
+
       </header>
     </div>
   );
